f7485655de5a35420452b3a3317ee8f5863a0c6e
mau2662 <vargas7384@gmail.com>
2023-06-27 18:16:08 -0600

    users

--- NewOptionStore/src/main/java/com/newOption/Encriptar.java
+++ NewOptionStore/src/main/java/com/newOption/Encriptar.java
@@ -5,9 +5,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class Encriptar {
     public static void main(String[] args) {
         var codigo = new BCryptPasswordEncoder();
-        System.out.println("juan (123): "+codigo.encode("123"));
-        System.out.println("rebeca (456): "+codigo.encode("456"));
-        System.out.println("pedro (789): "+codigo.encode("789"));
+        System.out.println("mauricio (123): "+codigo.encode("123"));
+        System.out.println("valeria (456): "+codigo.encode("456"));
+        System.out.println("armando (789): "+codigo.encode("789"));
         
     }
 }
